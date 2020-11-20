bd31674291fa7efbaaa120a50c7c697779d592f7
B-Stock Admin <jacobthomasmorris.com>
2020-11-19 22:39:58 -0700

    done

--- api/server.js
+++ api/server.js
@@ -6,4 +6,22 @@ const server = express();
 
 server.use(express.json());
 
+server.get('/', (req, res) =>{
+    db('accounts').then(r => res.send(r));
+})
+
+server.get('/:id', (req, res) =>{
+    db('accounts').where({id: req.params.id}).then(r => res.send(r));
+})
+server.post('/', (req, res) =>{
+    db('accounts').insert(req.body).then(r => res.send(`new record added with id of ${r}`)).catch(e=>res.send(e));
+})
 module.exports = server;
+
+server.put('/:id', (req, res) =>{
+    db('accounts').where({id: req.params.id}).update(req.body).then(r => res.send(`${r} record(s) updated`));
+})
+
+server.delete('/:id', (req, res) =>{
+    db('accounts').where({id: req.params.id}).del().then(r => res.send(`${r} record(s) deleted`));
+})
